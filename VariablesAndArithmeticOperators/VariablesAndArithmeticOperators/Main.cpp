75a5d2fe426e91d10fed758d8bfb615c48cde24c
Denis Rizov <d.b.rizov@gmail.com>
2014-04-25 20:13:43 +0300

    Variables and Arithmetic Operators Project Added

---
+++ VariablesAndArithmeticOperators/VariablesAndArithmeticOperators/Main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void main()
+{
+	string firstName = "";
+	cout << "Enter your first name: ";
+	cin >> firstName;
+	cout << "Hello, " << firstName << endl;
+}
